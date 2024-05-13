f26bcdc95a5886de3890f00849aaf5ab89537b1c
sachintha15985 <136817698+sachintha15985@users.noreply.github.com>
2024-05-13 18:46:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+    function searchFunction() {
+        var searchInput = document.getElementById("searchInput").value;
+        // You can perform the search functionality here
+        alert("Performing search for: " + searchInput);
+    }
+
+    // Define another function as needed
+    function anotherFunction() {
+        // Your code here
+        alert("Another function called.");
+    }
+    
+    
+   
